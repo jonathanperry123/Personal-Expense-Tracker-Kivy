263f12b43ef2107eed1514cb875953716c93c704
Python Perry <jonathan_perry_analytics@4jf1wm.onmicrosoft.com>
2024-05-25 15:21:10 -0500

    My first commit to GitHub

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from kivy.uix.label import Label
 from kivy.properties import NumericProperty
 from kivy.core.window import Window
 from kivy.uix.textinput import TextInput
+from kivy.app import App
 
 
 class UserInterface(BoxLayout):
