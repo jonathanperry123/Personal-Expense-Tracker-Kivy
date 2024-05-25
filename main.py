bc51f6699e0c7644d9e7b420452f6ea5d239849f
Python Perry <jonathan_perry_analytics@4jf1wm.onmicrosoft.com>
2024-05-25 15:01:46 -0500

    First Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from kivy.app import App 
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.properties import NumericProperty
+from kivy.core.window import Window
+from kivy.uix.textinput import TextInput
+
+
+class UserInterface(BoxLayout):
+    pass
+
+class ExpenseTracker(App):
+    pass
+
+ExpenseTracker().run()
